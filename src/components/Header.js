1bdceb3c31e9d88b8867e41461f0ffbb29267e41
Andres <andha037@gmail.com>
2020-12-04 20:31:22 -0300

    adding responsive layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import logo from './logo-1.svg';
 
 const Header = () => {
     return(
-        <header className="App-header">
+        <header className="app-header">
             <h1>Unit Converter App</h1>
             <figure className="logo">
                 <img className="logo-svg" src={logo} alt="AWD-logo"/>
